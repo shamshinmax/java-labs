786cb99341733a81017cc1b7a8ece481e4600a34
Макс Шамшин <94185311+shamshinmax@users.noreply.github.com>
2026-04-15 00:07:31 +0300

    add intContainer

---
+++ 1/src/main/java/lab1/IntContainer.java
@@ -0,0 +1,42 @@
+package lab1;
+
+public class IntContainer {
+    private int[] data = new int[10];
+    private int size;
+
+    public void add(int value) {
+        if (size == data.length) {
+            int[] temp = new int[data.length * 2];
+            for (int i = 0; i < size; i++) {
+                temp[i] = data[i];
+            }
+            data = temp;
+        }
+        data[size++] = value;
+    }
+
+    public int get(int index) {
+        checkIndex(index);
+        return data[index];
+    }
+
+    public int remove(int index) {
+        checkIndex(index);
+        int value = data[index];
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        size--;
+        return value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+}
--- 1/src/main/java/lab1/Main.java
+++ 1/src/main/java/lab1/Main.java
@@ -2,6 +2,17 @@ package lab1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
+        IntContainer container = new IntContainer();
+
+
+        container.add(10);
+        container.add(20);
+        container.add(30);
+
+
+        System.out.println("Второй: " + container.get(1));
+        System.out.println("Удаленный: " + container.remove(0));
+        System.out.println("Размер: " + container.size());
     }
 }
